b7d90dee31ea51a0cd6ddc968f85a9d01eaef205
rxhuang <rxhuang@live.ca>
2020-12-06 22:24:36 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,6 +72,7 @@ def show_visualization():
     ids = [2,1,5,4,6,8,9,11,10,12,13,15,19,16,17,18,20,21,22,25,24,23,26,27,29,28,30,37,38,31,33,34,35,32,
            36,39,40,41,42,44,45,46,47,48,49,51,50,53,55,54,56] 
     map_data['id'] = ids
+    map_data['unmasked_percentage'] = 100-map_data['mask_percentage']
 
     states = alt.topo_feature(data.us_10m.url, 'states')
     variable_list = ['mask_percentage','sympton_percentage']
